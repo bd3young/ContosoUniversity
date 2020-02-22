cc1c6482d037b3eda029593555e939a26cd5edde
Beau DeYoung <beaudeyoung@gmail.com>
2020-02-22 16:33:29 -0500

    Push 2

---
+++ Migrations/202002222049075_DattaAnnotations.cs
@@ -0,0 +1,18 @@
+﻿namespace ContosoUniversity.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DattaAnnotations : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.StudentInfo", "Phone", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.StudentInfo", "Phone");
+        }
+    }
+}
